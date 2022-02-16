0ecb53faf3c6bac64802dfe74e7c5de853d0cc99
Jaxque <jackyyotsuba@gmail.com>
2022-02-15 20:02:41 -0500

    updated

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const ABS_STATIC_PATH   = __dirname + '/public';
 
 //set our routes
 app.get('/', function (req, res) {
-    res.sendFile('index.html', {root:ABS_STATIC_PATH});
+    res.sendFile('main.html', {root:ABS_STATIC_PATH});
 });
 
 server.listen(LISTEN_PORT);                         //starts server
