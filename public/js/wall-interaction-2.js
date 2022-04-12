287b4a501f94785e6641e22eb7e3d79e4ed1fdde
angelinabelingan <80114685+angelinabelingan@users.noreply.github.com>
2022-04-12 18:13:52 -0400

    Added beaver story

--- public/js/wall-interaction-2.js
+++ public/js/wall-interaction-2.js
@@ -1,45 +1,26 @@
 'use strict';
 
 
-AFRAME.registerComponent('wall-interaction-2', { 
+AFRAME.registerComponent('beaver-02-appear', { 
   init: function() {
     const Context_AF = this;
 
-    let scene = document.querySelector('a-scene');
-
-    //create entity 
-    let entityBox  = document.createElement('a-entity')
+    Context_AF.beaver02      = document.getElementById("Beaver_02");
+    Context_AF.isVisible = false;
     
     //listen on click
-    Context_AF.el.addEventListener('mousedown', function() {
-
-      if (document.getElementById("pop_art2")) {
-        //remove shape from scene 
-        console.log('removed');
-        scene.removeChild(entityBox);
-
-        //remove shape attributes
-        entityBox.removeAttribute('geometry');
-        entityBox.removeAttribute('position');
-        entityBox.removeAttribute('material');
-      }
-      else if (!document.getElementById("pop_art2")){
-        //add shape to scene 
-        console.log('added');
-        scene.appendChild(entityBox);
-
-        //set shape attributes 
-        entityBox.setAttribute('geometry', {
-          primitive: 'box',
-          height: 0.5,
-          width: 0.5,
-          depth: 0.5
-        });
-        entityBox.setAttribute('position', {x: -28, y: 2, z: -40});
-        entityBox.setAttribute('material', 'color', 'blue');
-        entityBox.setAttribute('id', "pop_art2");
-      }
-    });
+    Context_AF.el.addEventListener('click', function() {
+        if (Context_AF.isVisible === true) {
+          console.log('invisible');
+          Context_AF.beaver02.setAttribute('visible', {enabled:false});
+          Context_AF.isVisible = false;
+        }
+        else {
+          console.log('visible');
+          Context_AF.beaver02.setAttribute('visible', {enabled:true});
+          Context_AF.isVisible = true;
+        }
+      });
   }
 
 });
